50be19f2fd282fe5789b8fde00efe654a6823389
AEydi <abdollah.eydi@gmail.com>
2021-10-01 19:49:01 +0330

    update googletrans

--- googletrans/__init__.py
+++ googletrans/__init__.py
@@ -1,7 +1,9 @@
 """Free Google Translate API for Python. Translates totally free of charge."""
-__all__ = 'Translator',
+__all__ = 'TextTranslator', 'WordTranslator'
 __version__ = '3.1.0-alpha'
 
 
-from googletrans.client import Translator
+from googletrans.client import TextTranslator
+from googletrans.clientN import WordTranslator
 from googletrans.constants import LANGCODES, LANGUAGES  # noqa
+from googletrans.constantsN import LANGCODES, LANGUAGES  # noqa
